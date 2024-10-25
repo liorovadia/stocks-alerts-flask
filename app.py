4b711f09374885efdd279044a0d605e9a24e087d
unknown <liorgalovadia@gmail.com>
2024-10-25 15:53:47 +0300

    changed to basic server to test that it's working proparly first

--- app.py
+++ app.py
@@ -1,25 +1,26 @@
 from flask import Flask, jsonify, request
-import yfinance as yf
+# import yfinance as yf
 
 app = Flask(__name__)
 
 @app.route('/stock', methods=['GET'])
 def get_stock_data():
-    symbol = request.args.get('symbol')
-    if not symbol:
-        return jsonify({"error": "Stock symbol is required"}), 400
+    return "<h1>Lior's Flask test</h1>"
+    # symbol = request.args.get('symbol')
+    # if not symbol:
+    #     return jsonify({"error": "Stock symbol is required"}), 400
 
-    try:
-        stock = yf.Ticker(symbol)
-        stock_info = stock.history(period="1d")
-        current_price = stock_info['Close'][0]
+    # try:
+    #     stock = yf.Ticker(symbol)
+    #     stock_info = stock.history(period="1d")
+    #     current_price = stock_info['Close'][0]
 
-        return jsonify({
-            "symbol": symbol,
-            "price": current_price
-        })
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
+    #     return jsonify({
+    #         "symbol": symbol,
+    #         "price": current_price
+    #     })
+    # except Exception as e:
+    #     return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
     app.run()
