9bc9a782201a981c9fb641a97f8c3f2c5939af4e
unknown <liorgalovadia@gmail.com>
2024-10-26 12:24:29 +0300

    CORS Access-Control-Allow-Origin should be added to response header

--- app.py
+++ app.py
@@ -5,8 +5,11 @@ import requests
 import pandas as pd
 
 app = Flask(__name__)
-CORS(app)  # Enable CORS for all routes
+# CORS(app)  # Enable CORS for all routes
 # CORS(app, origins=["http://localhost:3000"]) # allow requests only from localhost:3000
+CORS(app, resources={r"/*": {"origins": "*"}}, supports_credentials=True, 
+     expose_headers=["Content-Type", "Authorization", "Access-Control-Allow-Origin"],
+     allow_headers=["Content-Type", "Authorization", "Access-Control-Allow-Origin"])
 
 @app.route('/data')
 def fetch_data():
